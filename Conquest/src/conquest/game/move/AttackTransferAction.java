8d02d8bcae018896dbe120a5c8894e4fc2ea9bef
Adam Dingle <adam@medovina.org>
2019-11-09 05:19:26 +0100

    Rename move classes

--- Conquest/src/conquest/game/move/AttackTransferAction.java
+++ Conquest/src/conquest/game/move/AttackTransferAction.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import conquest.game.GameState;
 
-public class MoveAction implements Action {
+public class AttackTransferAction implements Action {
     public List<AttackTransferMove> commands;
     
-    public MoveAction(List<AttackTransferMove> commands) { this.commands = commands; }
+    public AttackTransferAction(List<AttackTransferMove> commands) { this.commands = commands; }
     
     public void apply(GameState state) {
         state.attackTransfer(commands);
--- Conquest/src/conquest/game/move/PlaceArmiesAction.java
+++ Conquest/src/conquest/game/move/PlaceArmiesAction.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import conquest.game.GameState;
 
-public class PlaceAction implements Action {
+public class PlaceArmiesAction implements Action {
     public List<PlaceArmiesMove> commands;
     
-    public PlaceAction(List<PlaceArmiesMove> commands) { this.commands = commands; }
+    public PlaceArmiesAction(List<PlaceArmiesMove> commands) { this.commands = commands; }
     
     public void apply(GameState state) {
         state.placeArmies(commands);
